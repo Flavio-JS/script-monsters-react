3890abffaf7bed6ec79a2c47719d5b6e8c75cabf
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-07-25 18:54:20 -0300

    add componente 'How To Play'

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import "./App.css";
+import { useState } from "react";
 import { Header } from "./components/Header/Header";
+import { HowToPlay } from "./components/HowToPlay/HowToPlay";
+
+import "./App.css";
 
 function App() {
+  let [howToPlay, setHTP] = useState(true);
+
+  let textButton: string = "Próximo";
   return (
-    <div>
+    <div className="container">
       <Header></Header>
+      {howToPlay && (
+        <HowToPlay>
+          <button>Próximo</button>
+        </HowToPlay>
+      )}
     </div>
   );
 }
---
+++ src/components/HowToPlay/HowToPlay.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+import "./HowToPlay.css";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const HowToPlay = ({ children }: Props) => {
+  return (
+    <section className="how-to-play">
+      <div className="play-area">
+        <h2>Como Jogar</h2>
+        <ul>
+          <li>Escolha seu ScriptMonster</li>
+          <li>Durante o jogo escolha uma das 3 ações</li>
+          <li>
+            <ul id="how-to-play-skills">
+              Cada ScriptMonster tem 3 habilidades
+              <li>Mordida (ataque neutro)</li>
+              <li>Skill (ataque elemental)</li>
+              <li>Curar (cura HP)</li>
+            </ul>
+          </li>
+        </ul>
+        {children}
+      </div>
+    </section>
+  );
+};
