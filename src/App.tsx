f005dd9a4938826311170cf2c71ae2c03e65f898
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-07-29 10:38:51 -0300

    Criado Componente Battle Time (in progress)

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,25 @@ import { Button } from "./components/Button/Button";
 import { ChooseMonster } from "./components/ChooseMonster/ChooseMonster";
 import { playerStatus } from "./Types/playerStatus";
 import { NextTurn } from "./components/NextTurn/NextTurn";
+import { BattleTime } from "./components/BattleTime/BattleTime";
 
 import "./App.css";
 
 function App() {
+  /* Use States */
   let [howToPlay, setHowToPlay] = useState(true);
   let [player1ChooseMonster, setPlayer1ChooseMonster] = useState(false);
-  let [player2Turn, setPlayer2Turn] = useState(false);
   let [player2ChooseMonster, setPlayer2ChooseMonster] = useState(false);
-  let [battleTime, setBattleTime] = useState(false);
+  let [player1Turn, setPlayer1Turn] = useState(false);
+  let [player2Turn, setPlayer2Turn] = useState(false);
+  let [battleTimeP2, setBattleTimeP2] = useState(false);
+  let [battleTimeP1, setBattleTimeP1] = useState(false);
+  let [turn, setTurn] = useState(0);
+  let [log1, setlog1] = useState("Começou o jogo");
+  let [log2, setlog2] = useState("");
+  let [statusImgP1Position, SetStatusImgP1Position] = useState(2);
+  let [statusImgP2Position, SetStatusImgP2Position] = useState(2);
+
   let [player1, setPlayer1] = useState({
     ID: "player1",
     HP: 0,
@@ -41,16 +51,20 @@ function App() {
     Fraco: "",
   });
 
+  /* funções */
+
   let passHowToPlay = () => {
     setHowToPlay(false);
     setPlayer1ChooseMonster(true);
   };
-  let choicePlayer1 = (e: playerStatus) => {
+
+  let choicePlayer1 = (monsterStatus: playerStatus) => {
     setPlayer1ChooseMonster(false);
-    setPlayer1(e);
+    setPlayer1(monsterStatus);
     setPlayer2Turn(true);
   };
-  let passChoiceMonster = () => {
+
+  let passChoiceMonsterP1 = () => {
     setPlayer2Turn(false);
     setPlayer2ChooseMonster(true);
   };
@@ -58,20 +72,72 @@ function App() {
     setPlayer1ChooseMonster(true);
     setPlayer2Turn(false);
   };
-  let choicePlayer2 = (e: playerStatus) => {
+
+  let choicePlayer2 = (monsterStaus: playerStatus) => {
     setPlayer2ChooseMonster(false);
-    setPlayer2(e);
-    setBattleTime(true);
+    setPlayer2(monsterStaus);
+    setPlayer1Turn(true);
   };
+
+  let passChoiceMonsterP2 = () => {
+    setPlayer1Turn(false);
+    setBattleTimeP1(true);
+  };
+  let changeChoiceP2 = () => {
+    setPlayer2ChooseMonster(true);
+    setPlayer1Turn(false);
+  };
+
   let backToHowToPlay = () => {
     setPlayer1ChooseMonster(false);
     setPlayer2ChooseMonster(false);
     setHowToPlay(true);
   };
 
-  let textButtonHowToPlay: string = "Próximo";
-  let textButtonBack: string = "Voltar";
-  let textButtonNextTurn: string = "Passar";
+  let battleTurnP1 = () => {
+    setBattleTimeP1(false);
+    setBattleTimeP2(true);
+    console.log("clicou");
+  };
+  let battleTurnP2 = () => {
+    setBattleTimeP2(false);
+    setBattleTimeP1(true);
+  };
+
+  let setLogs = (
+    turn: number,
+    log1: string,
+    log2: string,
+    updateImgP1: number,
+    updateImgP2: number
+  ) => {
+    setTurn(turn);
+    setlog1(log1);
+    setlog2(log2);
+    SetStatusImgP1Position(updateImgP1);
+    SetStatusImgP2Position(updateImgP2);
+  };
+
+  /* variáveis */
+
+  let textButtonHowToPlay = "Próximo";
+  let textButtonBack = "Voltar";
+  let textButtonNextTurn = "Passar";
+
+  let statusImgP1 = [
+    "biting",
+    "lost",
+    "normal",
+    "skill-to-right",
+    "take-damage",
+  ];
+  let statusImgP2 = [
+    "biting",
+    "lost",
+    "normal",
+    "skill-to-left",
+    "take-damage",
+  ];
 
   /* status */
 
@@ -115,10 +181,6 @@ function App() {
     Fraco: "Fire",
   };
 
-  console.log(`Player1\n
-    ID: ${player1.ID}\n
-  `);
-
   return (
     <div className="container">
       <Header></Header>
@@ -129,7 +191,6 @@ function App() {
       )}
       {player1ChooseMonster && (
         <ChooseMonster
-          player={player1}
           text="Player 1"
           monsters={[fireMonsterStatus, iceMonsterStatus, aquaMonsterStatus]}
           choiceFn={choicePlayer1}
@@ -139,12 +200,16 @@ function App() {
       )}
       {player2Turn && (
         <NextTurn text="Player 2" fn={changeChoiceP1}>
-          <Button text={textButtonNextTurn} clickfn={passChoiceMonster} />
+          <Button text={textButtonNextTurn} clickfn={passChoiceMonsterP1} />
+        </NextTurn>
+      )}
+      {player1Turn && (
+        <NextTurn text="Player 1" fn={changeChoiceP2}>
+          <Button text={textButtonNextTurn} clickfn={passChoiceMonsterP2} />
         </NextTurn>
       )}
       {player2ChooseMonster && (
         <ChooseMonster
-          player={player2}
           text="Player 2"
           monsters={[fireMonsterStatus, iceMonsterStatus, aquaMonsterStatus]}
           choiceFn={choicePlayer2}
@@ -152,7 +217,38 @@ function App() {
           <Button text={textButtonBack} clickfn={backToHowToPlay} />
         </ChooseMonster>
       )}
-      {battleTime && <div>ble ble</div>}
+      {battleTimeP1 && (
+        <BattleTime
+          player={player1}
+          enemy={player2}
+          text="Player 1"
+          turn={turn}
+          log1={log1}
+          log2={log2}
+          fn={battleTurnP1}
+          fnLog={setLogs}
+          statusImgP1={statusImgP1}
+          statusImgP2={statusImgP2}
+          statusImgP1Position={statusImgP1Position}
+          statusImgP2Position={statusImgP2Position}
+        ></BattleTime>
+      )}
+      {battleTimeP2 && (
+        <BattleTime
+          player={player2}
+          enemy={player1}
+          text="Player 2"
+          turn={turn}
+          log1={log1}
+          log2={log2}
+          fn={battleTurnP2}
+          fnLog={setLogs}
+          statusImgP1={statusImgP1}
+          statusImgP2={statusImgP2}
+          statusImgP1Position={statusImgP1Position}
+          statusImgP2Position={statusImgP2Position}
+        ></BattleTime>
+      )}
     </div>
   );
 }
---
+++ src/components/BattleTime/BattleTime.tsx
@@ -0,0 +1,358 @@
+import { useState } from "react";
+import { playerStatus } from "../../Types/playerStatus";
+import { Alert } from "../Alert/Alert";
+import "./BattleTime.css";
+
+type Props = {
+  player: playerStatus;
+  enemy: playerStatus;
+  text: string;
+  turn: number;
+  log1: string;
+  log2: string;
+  fn: () => any;
+  fnLog: (
+    turn: number,
+    log1: string,
+    log2: string,
+    updateImgP1: number,
+    updateImgP2: number
+  ) => any;
+  statusImgP1: string[];
+  statusImgP2: string[];
+  statusImgP1Position: number;
+  statusImgP2Position: number;
+};
+
+export const BattleTime = ({
+  player,
+  text,
+  enemy,
+  turn,
+  log1,
+  log2,
+  fn,
+  fnLog,
+  statusImgP1,
+  statusImgP2,
+  statusImgP1Position,
+  statusImgP2Position,
+}: Props) => {
+  let [alert, setAlert] = useState(false);
+  let [action, setAction] = useState(1);
+
+  const onClickBite = () => {
+    if (action === 1) {
+      bite();
+      setAction(0);
+      turn++;
+      fnLog(turn, log1, log2, statusImgP1Position, statusImgP2Position);
+      setTimeout(() => {
+        fn();
+      }, 2000);
+    }
+  };
+  const bite = () => {
+    let hitChanceDice = Math.floor(Math.random() * 100);
+    let criticalChanceDice = Math.floor(Math.random() * 100);
+
+    if (hitChanceDice > enemy.EsquivaChance) {
+      if (criticalChanceDice <= player.CritChance) {
+        enemy.HP = enemy.HP - player.NormalAtk * 1.5;
+        player.Stamina = player.Stamina + 50;
+        if (player.Stamina > 100) {
+          player.Stamina = 100;
+        }
+        if (text === "Player 1") {
+          log1 = `Você acertou um CRÍTICO e causou ${
+            player.NormalAtk * 1.5
+          } de dano`;
+          log2 = `Você sofreu um CRÍTICO e sofreu ${
+            player.NormalAtk * 1.5
+          } de dano`;
+        } else {
+          log2 = `Você acertou um CRÍTICO e causou ${
+            player.NormalAtk * 1.5
+          } de dano`;
+          log1 = `Você sofreu um CRÍTICO e sofreu ${
+            player.NormalAtk * 1.5
+          } de dano`;
+        }
+      } else {
+        enemy.HP = enemy.HP - player.NormalAtk;
+        player.Stamina = player.Stamina + 50;
+        if (player.Stamina > 100) {
+          player.Stamina = 100;
+        }
+
+        if (text === "Player 1") {
+          log1 = `Você acertou um ataque normal e causou ${player.NormalAtk} de dano`;
+
+          log2 = `Você sofreu um ataque normal e sofreu ${player.NormalAtk} de dano`;
+        } else {
+          log2 = `Você acertou um ataque normal e causou ${player.NormalAtk} de dano`;
+
+          log1 = `Você sofreu um ataque normal e sofreu ${player.NormalAtk} de dano`;
+        }
+      }
+      if (text === "Player 1") {
+        statusImgP1Position = 0;
+        statusImgP2Position = 4;
+      } else {
+        statusImgP2Position = 0;
+        statusImgP1Position = 4;
+      }
+      /*
+          0-biting 1-lost 2-normal 3-skill 4-take-damage
+        */
+    } else {
+      enemy.Stamina = enemy.Stamina + 25;
+      if (enemy.Stamina > 100) {
+        enemy.Stamina = 100;
+      }
+
+      if (text === "Player 1") {
+        log1 = `O inimigo ESQUIVOU do seu ataque`;
+        log2 = `Você ESQUIVOU do ataque inimigo`;
+      } else {
+        log2 = `O inimigo ESQUIVOU do seu ataque`;
+        log1 = `Você ESQUIVOU do ataque inimigo`;
+      }
+
+      if (text === "Player 1") {
+        statusImgP1Position = 0;
+        statusImgP2Position = 2;
+      } else {
+        statusImgP2Position = 0;
+        statusImgP1Position = 2;
+      }
+      /*
+          0-biting 1-lost 2-normal 3-skill 4-take-damage
+        */
+    }
+  };
+
+  const onClickSkill = () => {
+    if (action === 1) {
+      if (player.Stamina === 100) {
+        skill();
+        setAction(0);
+        turn++;
+        fnLog(turn, log1, log2, statusImgP1Position, statusImgP2Position);
+        setTimeout(() => {
+          fn();
+        }, 2000);
+      } else {
+        setAlert(true);
+        setTimeout(() => {
+          setAlert(false);
+        }, 2000);
+      }
+    }
+  };
+  const skill = () => {
+    let hitChanceDice = Math.floor(Math.random() * 100);
+    let criticalChanceDice = Math.floor(Math.random() * 100);
+
+    player.Stamina = 0;
+    if (hitChanceDice > enemy.EsquivaChance) {
+      if (criticalChanceDice <= player.CritChance) {
+        if (player.Forte === enemy.Fraco) {
+          enemy.HP = enemy.HP - player.SkillAtk * 1.5 * 1.05;
+
+          if (text === "Player 1") {
+            log1 = `Você acertou uma SKILL CRÍTICA e causou ${
+              player.SkillAtk * 1.5 * 1.05
+            } de dano`;
+
+            log2 = `Você sofreu uma SKILL CRÍTICO e sofreu ${
+              player.SkillAtk * 1.5 * 1.05
+            } de dano`;
+          } else {
+            log2 = `Você acertou uma SKILL CRÍTICA e causou ${
+              player.SkillAtk * 1.5 * 1.05
+            } de dano`;
+
+            log1 = `Você sofreu uma SKILL CRÍTICO e sofreu ${
+              player.SkillAtk * 1.5 * 1.05
+            } de dano`;
+          }
+        } else {
+          enemy.HP = enemy.HP - player.SkillAtk * 1.5;
+
+          if (text === "Player 1") {
+            log1 = `Você acertou uma SKILL e causou ${
+              player.SkillAtk * 1.5
+            } de dano`;
+
+            log2 = `Você sofreu uma SKILL e sofreu ${
+              player.SkillAtk * 1.5
+            } de dano`;
+          } else {
+            log2 = `Você acertou uma SKILL e causou ${
+              player.SkillAtk * 1.5
+            } de dano`;
+
+            log1 = `Você sofreu uma SKILL e sofreu ${
+              player.SkillAtk * 1.5
+            } de dano`;
+          }
+        }
+      } else {
+        if (player.Forte === enemy.Fraco) {
+          enemy.HP = enemy.HP - player.SkillAtk * 1.05;
+
+          if (text === "Player 1") {
+            log1 = `Você acertou uma SKILL CRÍTICA e causou ${
+              player.SkillAtk * 1.05
+            } de dano`;
+
+            log2 = `Você sofreu uma SKILL CRÍTICO e sofreu ${
+              player.SkillAtk * 1.05
+            } de dano`;
+          } else {
+            log2 = `Você acertou uma SKILL CRÍTICA e causou ${
+              player.SkillAtk * 1.05
+            } de dano`;
+
+            log1 = `Você sofreu uma SKILL CRÍTICO e sofreu ${
+              player.SkillAtk * 1.05
+            } de dano`;
+          }
+        } else {
+          enemy.HP = enemy.HP - player.SkillAtk;
+
+          if (text === "Player 1") {
+            log1 = `Você acertou uma SKILL e causou ${player.SkillAtk} de dano`;
+            log2 = `Você sofreu uma SKILL e sofreu ${player.SkillAtk} de dano`;
+          } else {
+            log2 = `Você acertou uma SKILL e causou ${player.SkillAtk} de dano`;
+            log1 = `Você sofreu uma SKILL e sofreu ${player.SkillAtk} de dano`;
+          }
+        }
+      }
+      if (text === "Player 1") {
+        statusImgP1Position = 3;
+        statusImgP2Position = 4;
+      } else {
+        statusImgP2Position = 3;
+        statusImgP1Position = 4;
+      }
+      /*
+          0-biting 1-lost 2-normal 3-skill 4-take-damage
+        */
+    } else {
+      enemy.Stamina = enemy.Stamina + 25;
+      if (enemy.Stamina > 100) {
+        enemy.Stamina = 100;
+      }
+
+      if (text === "Player 1") {
+        log1 = `O inimigo ESQUIVOU do seu ataque`;
+        log2 = `Você ESQUIVOU do ataque inimigo`;
+      } else {
+        log2 = `O inimigo ESQUIVOU do seu ataque`;
+        log1 = `Você ESQUIVOU do ataque inimigo`;
+      }
+
+      if (text === "Player 1") {
+        statusImgP2Position = 2;
+      } else {
+        statusImgP1Position = 2;
+      }
+      /*
+          0-biting 1-lost 2-normal 3-skill 4-take-damage
+        */
+    }
+    if (text === "Player 1") {
+      statusImgP1Position = 3;
+    } else {
+      statusImgP2Position = 3;
+    }
+    /*
+        0-biting 1-lost 2-normal 3-skill 4-take-damage
+      */
+  };
+
+  const onClickHeal = () => {
+    if (action === 1) {
+      if (player.Stamina >= 25) {
+        heal();
+        setAction(0);
+        turn++;
+        fnLog(turn, log1, log2, statusImgP1Position, statusImgP2Position);
+        setTimeout(() => {
+          fn();
+        }, 2000);
+      } else {
+        setAlert(true);
+        setTimeout(() => {
+          setAlert(false);
+        }, 2000);
+      }
+    }
+  };
+  const heal = () => {};
+
+  return (
+    <section className="battle-time">
+      {alert && <Alert text="Você não tem Stamina" />}
+      <div className="play-area">
+        <div className="battle-time-play-items">
+          <h2>{text}</h2>
+          {text === "Player 1" && (
+            <div
+              className="battle-time-monster-img"
+              style={{
+                backgroundImage: `url(${player.ImgsDirectory}${statusImgP1[statusImgP1Position]}.png)`,
+              }}
+            ></div>
+          )}
+          {text === "Player 2" && (
+            <div
+              className="battle-time-monster-img"
+              style={{
+                backgroundImage: `url(${player.ImgsDirectory}${statusImgP2[statusImgP2Position]}.png)`,
+              }}
+            ></div>
+          )}
+
+          <div className="battle-time-status">
+            <div className="battle-time-status-hp">
+              <i>HP: {player.HP}/2000</i>
+              <div className="battle-time-status-border-bar-hp">
+                <div
+                  className="battle-time-status-bar-hp"
+                  style={{
+                    width: `${(player.HP / 2000) * 100}%`,
+                  }}
+                ></div>
+              </div>
+            </div>
+
+            <div className="battle-time-status-stamina">
+              <i>Stamina: {player.Stamina}/100</i>
+              <div className="battle-time-status-border-bar-stamina">
+                <div
+                  className="battle-time-status-bar-stamina"
+                  style={{
+                    width: `${(player.Stamina / 100) * 100}%`,
+                  }}
+                ></div>
+              </div>
+            </div>
+          </div>
+          <div className="battle-time-log">
+            turno {turn}: {text === "Player 1" && <b>{log1}</b>}
+            {text === "Player 2" && <b>{log2}</b>}
+          </div>
+          <div className="battle-time-actions">
+            <button onClick={onClickBite}>Morder</button>
+            <button onClick={onClickSkill}>Skill</button>
+            <button onClick={onClickHeal}>Curar</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
