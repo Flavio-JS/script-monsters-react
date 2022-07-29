819e75fdd370ee016448371a49274eaf9396868f
Flavio Jeronimo da Silva Filho <flaviofilho.js@gmail.com>
2022-07-29 16:41:54 -0300

    Add component YouWin

---
+++ src/components/YouWin/YouWin.tsx
@@ -0,0 +1,50 @@
+import "./YouWin.css";
+
+type Props = {
+  text: string;
+  player1Img: string;
+  player2Img: string;
+};
+
+export const YouWin = ({ text, player1Img, player2Img }: Props) => {
+  return (
+    <section className="you-win">
+      <div className="play-area">
+        {text === "Player 1" && (
+          <>
+            <h1>{text} GANHOU !</h1>
+            <div
+              className="winer-img"
+              style={{
+                backgroundImage: `url(${player1Img}normal.png)`,
+              }}
+            ></div>
+            <div
+              className="loser-img"
+              style={{
+                backgroundImage: `url(${player2Img}lost.png)`,
+              }}
+            ></div>
+          </>
+        )}
+        {text === "Player 2" && (
+          <>
+            <h1>{text} GANHOU !</h1>
+            <div
+              className="winer-img"
+              style={{
+                backgroundImage: `url(${player2Img}normal.png)`,
+              }}
+            ></div>
+            <div
+              className="loser-img"
+              style={{
+                backgroundImage: `url(${player1Img}lost.png)`,
+              }}
+            ></div>
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
